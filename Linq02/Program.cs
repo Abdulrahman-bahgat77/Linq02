b0503c513356f82d1b8d4a431dd52ad11f349a29
abdulrahman <abdulrahman403868@fci.bu.edu.eg>
2025-02-05 21:29:02 +0200

    Add project files.

---
+++ Linq02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Linq02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Element Operator
+
+
+            #endregion
+        }
+    }
+}
